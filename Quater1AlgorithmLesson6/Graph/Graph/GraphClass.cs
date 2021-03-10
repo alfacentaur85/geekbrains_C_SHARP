c88763c6d0f3505a0c36053c42669a3b6cb3eedf
alfacentaur <alfacentaur@yandex.ru>
2021-03-10 13:01:57 +0300

    Algorithm-Lesson6
    Algorithm-Lesson6

---
+++ Quater1AlgorithmLesson6/Graph/Graph/GraphClass.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Graph
+{
+    static class GraphClass
+    {
+        public static int[,] adjMatrix { get; } =
+            {
+              { 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+              { 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+              { 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+              { 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+              { 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+              { 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+              { 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+              { 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+              { 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0 },
+              { 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0 },
+              { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+              { 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0 },
+              { 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0 },
+              { 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+              { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0 },
+              { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 },
+              { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1 },
+              { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1 },
+              { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 },
+              { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0 }
+            };
+
+        static bool[] visited; 
+
+        public static void BFS(int start)
+
+        {
+
+
+            visited = Enumerable.Repeat<bool>(false, adjMatrix.GetLength(0)).ToArray();
+
+            Queue<int> q = new Queue<int>();
+
+            q.Enqueue(start);
+
+
+            visited[0] = true;
+
+            int vis;
+
+            while (q.Count != 0)
+            {
+
+                vis = q.Dequeue();
+
+                Console.Write($"{vis} -> ");
+
+                for (int i = 0; i < adjMatrix.GetLength(0); i++)
+                {
+
+                    if (adjMatrix[vis, i] == 1 && (!visited[i]))
+                    {
+
+                        q.Enqueue(i);
+
+                        visited[i] = true;
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        public static void DFS(int start)
+        {
+            visited = Enumerable.Repeat<bool>(false, adjMatrix.GetLength(0)).ToArray();
+            DfsMethod(start);
+        }
+        static void DfsMethod(int start)
+
+        {
+            
+            Console.Write($"{start} -> ");
+
+            visited[start] = true;
+
+
+            for (int i = 0; i < adjMatrix.GetLength(0); i++)
+            {
+
+                if (adjMatrix[start, i] == 1 && (!visited[i]))
+                {
+
+                    DfsMethod(i);
+
+                }
+
+            }
+
+        }
+
+    }
+}
