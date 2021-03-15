d795ed008cedb08b6f5474ed97ed10e4ca8b2ffd
alfacentaur <alfacentaur@yandex.ru>
2021-03-15 12:42:30 +0300

    Algorithm-Lesson7
    Algorithm-Lesson7

---
+++ CountRoutes/CountRoutes/CountRoutesMN.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CountRoutes
+{
+    static class CountRoutesMN
+    {
+        public static int N { get; set; }
+        public static int M { get; set; }
+
+        public static int recursionCountRoutes(int m, int n)
+        {
+            if (m == 0 || n == 0)
+            {
+                return 0;
+            }
+            if (m == 1 && n == 1)
+            {
+                return 1;
+            }
+            return recursionCountRoutes(m, n - 1) + recursionCountRoutes(m - 1, n);
+
+        }
+    }
+}
